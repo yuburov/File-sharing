14f97db0043dbd11948a88fa424b1bd6382bde82
Yuburov <yubur.aziz@gmail.com>
2020-03-14 10:22:19 +0600

    Создана модель File и зарегистрировано в админ-панели, также добавлены тестовые данные и созадна фикстура

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from django.contrib import admin
+from webapp.models import File
 
-# Register your models here.
+
+admin.site.register(File)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class File(models.Model):
+    file = models.FileField(null= False, blank=False, upload_to='files', verbose_name='Фото')
+    caption = models.CharField(max_length=200, null=False, blank=False, verbose_name='Подпись')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    author = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL, related_name='files', verbose_name='Автор')
+
+
+    def __str__(self):
+        return self.caption
+
+    class Meta:
+        verbose_name = 'Файл'
+        verbose_name_plural = 'Файлы'
